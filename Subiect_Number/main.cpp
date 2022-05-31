741b6f120c48926142139b790ad6662868117ec4
anaungurean <100145653+anaungurean@users.noreply.github.com>
2022-05-31 20:26:14 +0300

    Create main.cpp

---
+++ Subiect_Number/main.cpp
@@ -0,0 +1,19 @@
+#include "Number.h"
+#include "Hexazecimal.h"
+#include "Decimal.h"
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	Number n1(1), n2(2), n3(10);
+	Number n4((int)((((n1 + n2) * n3) / n2) + (n1 - n2)));
+	(n4.operator+=(new Hexazecimal())) += (new Decimal());
+
+
+	n4.Print();
+	std::cout << "Valoarea hexazecimala este: 0x" << std::hex << std::setw(8) << std::setfill('0') << std::uppercase<<(int)n4 << std::endl;
+	
+   return 0;
+}
