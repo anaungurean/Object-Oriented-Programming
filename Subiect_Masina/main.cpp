2b32d1d8c5149a1c5337c639dfa171bd3ce79872
anaungurean <100145653+anaungurean@users.noreply.github.com>
2022-05-31 22:47:52 +0300

    Create main.cpp

---
+++ Subiect_Masina/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Masina.h"
+#include "MasinaOras.h"
+#include "Dacia.h"
+#include "Opel.h"
+#include "RangeRover.h"
+
+using namespace std;
+
+int main() {
+    Opel o;
+    o.setCapacitate(100);
+    o.setCuloare("rosu");
+    o.setAnFabricatie(2000);
+    MasinaOras *m = & o;
+    cout << m ->getName() << "," << m ->getCuloare() << "," << m ->getCapacitate() << "," << o.getAnFabricatie() << endl;
+    return 0;
+}
